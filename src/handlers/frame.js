b447b78e42209400044ee8d600bad63db585e40a
Joseph Pearson <joseph@inventivelabs.com.au>
2013-12-12 16:47:35 -0500

    Frame handler -- use innerHTML if no URL in data-pep-frame attribute.
    This code is copied-and-pasted from the quiz handler, so we should probably
    consider enshrining the pattern somewhere. Pep.Generate?

--- src/handlers/frame.js
+++ src/handlers/frame.js
@@ -9,15 +9,66 @@
 //
 Pep.Handler.Frame = function (pepdoc) {
   pepdoc.each('[data-pep-frame]', function (elem) {
-    elem.xPepActions = {
-      trigger: function () {
-        var url = elem.getAttribute('data-pep-frame');
-        var popopts = { className: 'pep-popup-frame', closeBtn: true };
-        Pep.Generate.popupIframe(elem, url, popopts);
-      }
-    };
-    elem.setAttribute('data-pep-send', 'trigger');
+    var url = elem.getAttribute('data-pep-frame');
+    if (url) {
+      elem.xPepActions = {
+        trigger: function () {
+          var popopts = { className: 'pep-popup-frame', closeBtn: true };
+          Pep.Generate.popupIframe(elem, url, popopts);
+        }
+      };
+      elem.setAttribute('data-pep-send', 'trigger');
+    } else if (elem.getAttribute('data-pep-frame-state') != 'live') {
+      Pep.Handler.Frame.Proxy(elem);
+    }
   });
+}
+
+
+Pep.Handler.Frame.Proxy = function (elem) {
+  var p = {};
+
+
+  function initialize() {
+    p.doc = elem.ownerDocument;
+    p.html = getFrameHTML();
+    p.stylesheet = elem.getAttribute('data-pep-frame-stylesheet');
+    p.button = replaceElement();
+    p.button.setAttribute('data-pep-send', 'trigger');
+    delete elem;
+  }
+
+
+  function getFrameHTML() {
+    var cloneElement = elem.cloneNode(true);
+    cloneElement.setAttribute('data-pep-frame-state', 'live');
+    var surrogate = p.doc.createElement('div');
+    surrogate.appendChild(cloneElement);
+    return surrogate.innerHTML;
+  }
+
+
+  function replaceElement() {
+    var btn = p.doc.createElement('input');
+    btn.type = 'hidden';
+    btn.id = elem.id;
+    elem.parentNode.insertBefore(btn, elem);
+    elem.parentNode.removeChild(elem);
+    btn.xPepActions = { 'trigger': trigger };
+    return btn;
+  }
+
+
+  function trigger(sender) {
+    Pep.Generate.popupIframe(
+      sender,
+      { fragment: p.html, stylesheet: p.stylesheet },
+      { className: 'pep-popup-frame', closeBtn: true }
+    );
+  }
+
+
+  initialize();
 }
 
 
